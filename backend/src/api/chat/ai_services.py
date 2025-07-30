33cd082fc52dd4c6871545e1fda947b26b0ed1db
deepmonapara9 <deepmonapara0@gmail.com>
2025-07-31 03:25:46 +0530

    Add structured output for LLM in ai_services.py by defining EmailMessage model and updating message handling for email generation.

--- backend/src/api/chat/ai_services.py
+++ backend/src/api/chat/ai_services.py
@@ -1,5 +1,14 @@
 import os
+import re
 from langchain_openai import ChatOpenAI
+from pydantic import BaseModel, Field
+
+
+class EmailMessage(BaseModel):
+    subject: str
+    contents: str
+    invalid_request: bool | None = Field(default=False)
+
 
 # Here we are setting the environment variables for the OpenAI API key and the model name
 OPENAI_BASE_URL = os.environ.get('OPENAI_BASE_URL') or None
@@ -18,3 +27,17 @@ if OPENAI_BASE_URL:
 
 # Here we are creating an instance of the ChatOpenAI class with the OpenAI parameters
 llm_base = ChatOpenAI(**openai_params)
+
+# This will be the structured output for the LLM
+llm = llm_base.with_structured_output(EmailMessage)
+
+messages = [
+    (
+        "system",
+        "You are a helpful assistant for research and composing plaintext emails. Do not use markdown in your response only plaintext.",
+    ),
+    ("human", "Create an email about the benefits of coffee. Do not use markdown in your response only plaintext"),
+]
+
+response = llm.invoke(messages)
+print(response)
