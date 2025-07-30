33b3f37670ec5dc96c7da84f0f59bf51cae6878c
deepmonapara9 <deepmonapara0@gmail.com>
2025-07-30 18:14:44 +0530

    Implement chat message creation endpoint in FastAPI and fix typo in ChatMessage model definition

--- backend/src/api/chat/models.py
+++ backend/src/api/chat/models.py
@@ -6,7 +6,7 @@ class ChatMessagePayLoad(SQLModel):
    message: str
 
 # This is a SQLModel class that represents a chat message in the database
-class ChatMessage(SQLModel, table=true):
+class ChatMessage(SQLModel, table=True):
    # Database table
    # It is for saving, updating, and deleting or getting the chat messages
    id: int | None = Field(default=None, primary_key=True)
--- backend/src/api/chat/routing.py
+++ backend/src/api/chat/routing.py
@@ -1,8 +1,25 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlmodel import Session
+from .models import ChatMessagePayLoad, ChatMessage
+from api.db import get_session
 
 router = APIRouter()
 
 # /api/chat/
 @router.get("/")
 def chat_health():
-   return {"status": "OK!"} 
+    return {"status": "OK!"}
+
+# HTTP POST -> payload = {"message": "hello world!"}
+@router.post("/", response_model=ChatMessage)
+def chat_create_message(
+    payload: ChatMessagePayLoad,
+    session: Session = Depends(get_session)
+):
+    data = payload.model_dump()  # pydantic -> dict
+    print(data)
+    obj = ChatMessage.model_validate(data)  # sqlmodel -> object
+    session.add(obj)
+    session.commit()
+    session.refresh(obj)
+    return obj
