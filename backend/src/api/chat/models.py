a17fbe4fd446121b948f545629a10dcd14c27d1a
deepmonapara9 <deepmonapara0@gmail.com>
2025-07-30 16:16:01 +0530

    Add ChatMessage and ChatMessagePayLoad models for chat message handling in the database

---
+++ backend/src/api/chat/models.py
@@ -0,0 +1,13 @@
+from sqlmodel import SQLModel, Field
+
+class ChatMessagePayLoad(SQLModel):
+   # Pydantic model 
+   # It is for validation
+   message: str
+
+# This is a SQLModel class that represents a chat message in the database
+class ChatMessage(SQLModel, table=true):
+   # Database table
+   # It is for saving, updating, and deleting or getting the chat messages
+   id: int | None = Field(default=None, primary_key=True)
+   message: str
