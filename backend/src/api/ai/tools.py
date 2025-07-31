237ce047949614fa101421f4957a48247c39dca0
deepmonapara9 <deepmonapara0@gmail.com>
2025-07-31 17:23:00 +0530

    Add email tools for sending and reading functionality in the AI module, including `send_me_email` and `get_unread_email` functions for email operations.

---
+++ backend/src/api/ai/tools.py
@@ -0,0 +1,49 @@
+from langchain_core.tools import tool
+
+from api.myemailer.sender import send_mail
+from api.myemailer.inbox_reader import read_inbox
+
+
+# This tool will be used to send mail
+@tool
+def send_me_email(subject: str, content: str) -> str:
+    """
+      Send an email to myself with a subject and content.
+
+      Arguments:
+      - subject: str - Text subject of the email
+      - content: str - Text body content of the email
+    """
+    try:
+        send_mail(subject=subject, content=content)
+    except:
+        return "Not sent"
+    return "Sent email"
+
+
+# This tool will be used to read emails
+@tool
+def get_unread_email(hours_ago: int = 48) -> str:
+    """
+      Read all emails from my inbox within the last N hours
+
+      Arguments:
+      - hours_ago: int = 24 - number of hours ago to retrieve in the inbox
+
+      Returns:
+      A string of emails separated by a line "----"
+    """
+    try:
+        emails = read_inbox(hours_ago=hours_ago, verbose=False)
+    except:
+        return "Error getting latest emails"
+    cleaned = []
+    for email in emails:
+        data = email.copy()
+        if "html_body" in data:
+            data.pop('html_body')
+        msg = ""
+        for k, v in data.items():
+            msg += f"{k}:\t{v}"
+        cleaned.append(msg)
+    return "\n-----\n".join(cleaned)[:500]
